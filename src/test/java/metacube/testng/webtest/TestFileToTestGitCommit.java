996543646f26c45e38f49ce22795fd49e74f390f
amitjhs <amitjhs2000@gmail.com>
2016-08-11 17:13:55 +0530

    Testing for Adding New File to Git

---
+++ src/test/java/metacube/testng/webtest/TestFileToTestGitCommit.java
@@ -0,0 +1,33 @@
+package metacube.testng.webtest;
+
+import metacube.testng.Asserts.MAssert;
+import metacube.testng.pages.GoogleSearchPage;
+import metacube.testng.properties.Prop;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class TestFileToTestGitCommit extends BaseTest{
+
+	String expectedTitle = "Metacube Software";
+	
+	GoogleSearchPage google;
+	
+	@BeforeClass
+	public void LaunchGoogleSearchPage()
+	{
+		// Comment to test Git Repository
+		google = new GoogleSearchPage(LaunchBrowser().LaunchApplication());
+		String url = Prop.GetProperty("Environment.properties", "BaseURL");
+	}
+	
+	@Test
+	public void LaunchGoogleSearchUI() {
+		google.Search(expectedTitle);
+		String actualTitle = driver.getTitle();
+		MAssert.assertAreEqual(actualTitle,"B");
+		
+	}
+	
+}
